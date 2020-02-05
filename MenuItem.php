7d2e671664af9c38db1dcf6a1b94b8b0faa08e53
phptheme <dev@getphptheme.com>
2020-02-05 18:38:58 +0300

    n/a

---
+++ MenuItem.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author PhpTheme Dev Team <dev@getphptheme.com>
+ * @license MIT
+ * @link http://getphptheme.com
+ */
+namespace PhpTheme\Core;
+
+class MenuItem extends \PhpTheme\Menu\MenuItem
+{
+}

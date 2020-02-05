6dc99a046b058f8a6bdd9e2da56b4c0dd5aef93e
phptheme <dev@getphptheme.com>
2020-02-05 17:48:30 +0300

    n/a

---
+++ Link.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author PhpTheme Dev Team <dev@getphptheme.com>
+ * @license MIT
+ * @link http://getphptheme.com
+ */
+namespace PhpTheme\Core;
+
+class Link extends \PhpTheme\Link\Link
+{
+}
